508d0384a7c5244b542956f76b4d95df0bc85d83
Jerry <jerry21yan@gmail.com>
2018-10-15 16:11:59 -0700

    Added vital documentation (VERY VERY IMPORTANT)

--- src/Expand.java
+++ src/Expand.java
@@ -3,6 +3,8 @@ public class Expand {
 	
 	public static void main(String[] args) {
 		System.out.println("expAnd");
+		//e̋̊̎̓͛͌̚xͭ̄͌͒̍p͌́̔̉̓̌̾̒̚ǎ͋͐̅ͭͮͩ̓n͊̓̓͒ͭͩ̚d̍̍̿̎̅̚
+		//p̱̠̲̱͙͓̗r̤̙̠̣e̗̫̲̗͖̘̻̬͈m̼͕̟̬̺͎͇i̳͓͉̖̼̙ͅu͔̙m̯̹̗̘ͅ ̫̗q̼͖u̫̪͖͍̹̯͓a̖̼̝̥͈̱l͇͍̻͈i͔̭̥̯̩̭̻̝t̥͉͖͉͉̻ỵ̹
 	}
 
 }
